1689a8e97142b61968f0afe062d81971a6a1970a
brad.jordan <brad.jordan@cloudability.com>
2017-01-18 14:24:45 -0700

    completed toggle feature

--- src/Container.js
+++ src/Container.js
@@ -4,22 +4,40 @@ import Section from './Section';
 export default class Container extends Component {
     constructor(props) {
         super(props)
+        this.state = {
+            openSectionIndex: 2
+        }
+        this.buildSection = this.buildSection.bind(this);
+        this.toggleOne = this.toggleOne.bind(this);
+    }
+
+    getInitialState(sectionList) {
+        return { openSectionIndex: -1 }
     }
 
     buildSections(sectionList) {
-        var sections = sectionList.map(function(section, index){
-            /* Remember to add a 'key'. React wants you to add an identifier when you instantiate a component multiple times */
-            return <Section key={index} data={section} />
-        })
+        var sections = sectionList.map(this.buildSection)
         return sections;
+    }
+
+    toggleOne(id) {
+        if(this.state.openSectionIndex === id){
+            this.setState({openSectionIndex: -1});
+        } else {
+            this.setState({openSectionIndex: id});
+        }
+    }
+
+    buildSection(section, index) {
+        var openStatus = (index === this.state.openSectionIndex);
+        /* Remember to add a 'key'. React wants you to add an identifier when you instantiate a component multiple times */
+        return <Section id={index} data={section} toggleOne={this.toggleOne} open={openStatus} />
     }
 
     render() {
         var sections = this.buildSections(this.props.data);
         return (
-            <div className="container">
-                {sections}
-            </div>
+            <div className="container">{sections}</div>
         );
     }
 }
--- src/Section.js
+++ src/Section.js
@@ -3,14 +3,31 @@ import React, {Component} from 'react';
 export default class Section extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            open: true,
+        }
+        this.toggleContent = this.toggleContent.bind(this);
+    }
+
+    getHeight() {
+        if(this.props.open){
+            return "3em"
+        } else {
+            return "0"
+        }
+    }
+
+    toggleContent() {
+        this.props.toggleOne(this.props.id)
     }
 
     render() {
+        var style = { height: this.getHeight() }
         return (
-            <div className={"section" + this.props.key}>
-                <h2 className="sectionTitle">{this.props.data.title}</h2>
-                <p className="sectionContent">{this.props.data.content}</p>
+            <div className={"section section" + this.props.key}>
+                <h2 className="sectionTitle" onClick={this.toggleContent} >{this.props.data.title}</h2>
+                <p className="sectionContent" style={style} >{this.props.data.content}</p>
             </div>
-        )
+        );
     }
 }
